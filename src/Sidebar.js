268c088455fc678d5ee57c0621baa45aed8d58ee
ToanHo <toanbku@gmail.com>
2020-09-05 17:04:39 +0700

    imp sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SidebarChat from './SidebarChat';
+
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src='https://avatars3.githubusercontent.com/u/28706996?s=460&u=71cf4f1fce961c2769dfe55414fd94d1371de7b5&v=4' />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="sidebar__headerLeft">
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCDUUujCLw0R_xrxTmKfjPA0grLing48r0",
+  authDomain: "whatsapp-mern-cb2c3.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-cb2c3.firebaseio.com",
+  projectId: "whatsapp-mern-cb2c3",
+  storageBucket: "whatsapp-mern-cb2c3.appspot.com",
+  messagingSenderId: "918372232288",
+  appId: "1:918372232288:web:89cb29fef7798c48daa705",
+  measurementId: "G-9VETVH2L6T"
+};
