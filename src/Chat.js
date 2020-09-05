ef281b1c53754c7b3b9a05450f935942708787ec
ToanHo <toanbku@gmail.com>
2020-09-05 18:07:18 +0700

    imp chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,73 @@
 import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import EmojiEmotionsOutlinedIcon from '@material-ui/icons/EmojiEmotionsOutlined';
+import MicNoneOutlinedIcon from '@material-ui/icons/MicNoneOutlined';
 import './Chat.css';
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am the chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        
+      </div>
+    
+      <div className="chat__footer">
+        <IconButton>
+          <EmojiEmotionsOutlinedIcon />
+        </IconButton>
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">
+            Send a message
+          </button>
+          <MicNoneOutlinedIcon />
+        </form>
+      </div>
     </div>
   )
 }
