71334770efafe1f0055f3a7e620ff55a049090f3
ToanHo <toanbku@gmail.com>
2020-09-13 21:47:54 +0700

    useEffect for subscribe pusher channel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('5edfc75639f0fa5977fb', {
+      cluster: 'ap1'
+    });
+
+    const channel = pusher.subscribe('message');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
